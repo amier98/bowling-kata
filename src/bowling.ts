800550b3eb2864001a6d7d23eb87350d8c6e0689
Amier Ali <amier_1998@hotmail.com>
2023-11-21 23:27:17 +0000

    feat: Implement number and miss score

--- src/bowling.ts
+++ src/bowling.ts
@@ -3,9 +3,16 @@ export function bowlingFrame(gameFrame: string): number {
   let rolls = 0;
 
   const gameRolls = gameFrame.split("");
-  console.log(gameRolls);
 
   const specialRolls: { [key: string]: number } = {
+    "-": 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
     "/": 10,
   };
   // a spare(/) is when both rolls in a frame equals 10 and plus the score of the next roll of next frame
@@ -17,14 +24,15 @@ export function bowlingFrame(gameFrame: string): number {
   //you cannot play more than 10 frames
 
   for (let frames = 0; frames < 10; frames++) {
+    const firstRoll = gameRolls[rolls];
+    const secondRoll = gameRolls[rolls + 1];
     if (gameRolls[rolls + 1] === "/") {
       rolls += 3;
       score += parseInt(gameRolls[rolls]) + specialRolls["/"];
     } else {
-      score += parseInt(gameRolls[rolls]) + parseInt(gameRolls[rolls + 1]);
+      score += specialRolls[firstRoll] + specialRolls[secondRoll];
+      rolls += 3;
     }
-    //rolls += 3;
   }
-
   return score;
 }
--- tests/bowling.test.ts
+++ tests/bowling.test.ts
@@ -3,9 +3,9 @@ import { bowlingFrame } from "../src/bowling";
 describe("frames", () => {
   it("return correct score for 10 frames", () => {
     //Arrange
-    const input = "11 11 11 11 11 11 11 11 11 11";
+    const input = "11 22 11 11 11 11 11 11 11 11";
     //Act
-    const expectedOutput = 20;
+    const expectedOutput = 22;
     //Assert
     expect(bowlingFrame(input)).toBe(expectedOutput);
   });
@@ -15,6 +15,15 @@ describe("frames", () => {
     const input = "5/ 7/ 7/ 7/ 7/ 7/ 7/ 7/ 7/ 73";
     //act
     const expectedOutput = 163;
+    //assert
+    expect(bowlingFrame(input)).toBe(expectedOutput);
+  });
+
+  it("return correct score of number and miss", () => {
+    //arrange
+    const input = "1- 1- 1- 1- 1- 1- 1- 1- 1- 1-";
+    //act
+    const expectedOutput = 10;
     //assert
     expect(bowlingFrame(input)).toBe(expectedOutput);
   });
